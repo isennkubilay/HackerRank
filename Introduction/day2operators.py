4607bf10355345a3bb27c386e2cd3a2e80c4ddac
Kubilay İşen <34842742+isennkubilay@users.noreply.github.com>
2018-11-08 23:38:29 +0100

    Add files via upload

---
+++ Introduction/day2operators.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+
+mealCost = float(input())  # original meal price
+tipPercent = int(input())  # tip percentage
+taxPercent = int(input())  # tax percentage
+
+# Write your calculation code here.
+cost = mealCost * (1 + (tipPercent + taxPercent) / 100.0)
+
+# cast the result of the rounding operation to an int and save it as totalCost
+totalCost = round(cost)
+
+# Print your result
+print(str(totalCost))
